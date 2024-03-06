0cfdcdf107f22bdab48b9eed7758491b3a76a45d
Thiago <thiiago.dev@gmail.com>
2024-03-06 07:53:33 -0300

    Inserindo a opção de escolher o stream do link RTSP.

--- assets/js/hikvision.js
+++ assets/js/hikvision.js
@@ -1,5 +1,5 @@
-function criarURLRTSP(usuario, senha, ip, porta, canal, substream) {
-    let url = `rtsp://${usuario}:${senha}@${ip}:${porta}/Streaming/Channels/${canal}0${substream}`;
+function criarURLRTSP(usuario, senha, ip, porta, stream, canal) {
+    let url = `rtsp://${usuario}:${senha}@${ip}:${porta}/Streaming/Channels/${canal}0${stream}`;
     return url;
 }
 
@@ -8,6 +8,7 @@ function gerarURLsRTSP() {
     let senha = document.getElementById('senha').value;
     let ip = document.getElementById('ip').value;
     let porta = document.getElementById('porta').value;
+    let stream = document.getElementById('stream').value;
     let canalInicial = parseInt(document.getElementById('canal').value, 10);
     let numCanais = parseInt(document.getElementById('numCanais').value, 10);
 
@@ -16,7 +17,7 @@ function gerarURLsRTSP() {
 
     for (let i = 0; i < numCanais; i++) {
         let canal = canalInicial + i;
-        let urlRTSP = criarURLRTSP(usuario, senha, ip, porta, canal, 1);
-        resultadosDiv.innerHTML += `<p>${urlRTSP}</p>`;
+        let urlRTSP = criarURLRTSP(usuario, senha, ip, porta, stream, canal);
+        resultadosDiv.innerHTML += `<span>${urlRTSP}</span><br>`;
     }
 }
