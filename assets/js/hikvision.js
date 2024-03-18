a0579a85903c40db24a0d637527caeebe2080402
Thiago <thiiago.dev@gmail.com>
2024-03-18 08:21:52 -0300

    Criando funções para a geração de nomes (com BUG)

--- assets/js/hikvision.js
+++ assets/js/hikvision.js
@@ -1,3 +1,4 @@
+// Funções e metodos para Gerar RTSPs
 function criarURLRTSP(usuario, senha, ip, porta, canal, substream) {
     let url = `rtsp://${usuario}:${senha}@${ip}:${porta}/Streaming/Channels/${canal}0${substream}`;
     return url;
@@ -11,12 +12,37 @@ function gerarURLsRTSP() {
     let canalInicial = parseInt(document.getElementById('canal').value, 10);
     let numCanais = parseInt(document.getElementById('numCanais').value, 10);
 
-    let resultadosDiv = document.getElementById('resultados');
-    resultadosDiv.innerHTML = '';
+    let resultadosRtspDiv = document.getElementById('resultados');
+    resultadosRtspDiv.innerHTML = '';
 
     for (let i = 0; i < numCanais; i++) {
         let canal = canalInicial + i;
         let urlRTSP = criarURLRTSP(usuario, senha, ip, porta, canal, 1);
-        resultadosDiv.innerHTML += `<p>${urlRTSP}</p>`;
+        resultadosRtspDiv.innerHTML += `<p>${urlRTSP}</p>`;
     }
+}
+
+// Funções e metodos para Gerar Nomes
+
+function criarNomes(organizacao, unidade, equipamento, camera){
+    let nomegerado = `${organizacao}-${unidade}-${equipamento}-${camera}`;
+    return nomegerado;
+}
+
+function gerarNomes() {
+    let organizacao = document.querySelector("#organizacao").value;
+    let unidade = document.querySelector("#unidade").value;
+    let equipamento = document.querySelector("#equipamento").value;
+    let cameraInicial = document.querySelector("#camera");
+    let numCameras = parseInt(document.querySelector("#numCameras").value);
+
+    let resultadosCamDiv = document.querySelector("resultados");
+    resultadosCamDiv.innerHTML = "";
+
+    for(let i = 1; < numCameras; i++) {
+        let camera = cameraInicial + i;
+        let nomeCamera = criarNomes(organizacao, unidade, equipamento, camera);
+        resultadosCamDiv.innerHTML += `<p>${nomeCamera}</p>`
+    }
+
 }
